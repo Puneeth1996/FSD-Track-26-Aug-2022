4dd9e2e293edb46de2d6018d982a854ddfb48764
Puneeth1996 <puneeth1996p@gmail.com>
2022-09-20 22:17:02 +0530

    learning on javascript

---
+++ Javascript/script.js
@@ -0,0 +1,9 @@
+function chnageToBlue() {
+    console.log('clicked on "Change to Blue" ');
+    document.getElementById('result1').style.color = 'blue';
+}
+
+function chnageToYellow() {
+    console.log('clicked on "Change to Violet" ');
+    document.getElementById('result2').style.color = 'violet';
+}
